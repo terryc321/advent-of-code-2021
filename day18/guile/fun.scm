87792285569cedb8b11f2d7c441c41d96864e39e
terry cadd <terry.cadd@gmail.com>
2025-05-10 00:03:09 +0100

    started day18

---
+++ day18/guile/fun.scm
@@ -0,0 +1,10 @@
+;; -*-scheme-*-
+
+
+;; thought it be cons but end up with dotted pairs but input.txt is given as
+;; list of 2 elements then nil as in (list 1 2)  -> 1 . (2 . nil)
+(define addition (x y)
+  (list x y))
+
+
+
